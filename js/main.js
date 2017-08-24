ac72a12366ce9898a114bb489a4f3163819c7b3a
Donavanrader <shaggyrader@gmail.com>
2017-08-25 00:34:42 +0100

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+
+document.getElementById("pageTitle").innerHTML = "Hellow World";
+
+var myString = "My awesome project kinda sucks...";
+
+var myElement = document.getElementById("pageSubTitle");
+
+myElement.innerHTML = myString;
+
+// console loggin'
+
+var w = 42;
+var x;
+
+var y = 42;
+
+var z = "42";
+
+var answer = w + z;
+var answer = y + z;
+
+console.info("Now we're whatever");
+console.log(myElement);
+console.log(myString);
+console.log(x);
+console.log(y);
+console.log("-------");
+console.log(answer);
+
