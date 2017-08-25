d24536cfd8ef4984762af34e89db9342caf9761d
Donavanrader <shaggyrader@gmail.com>
2017-08-25 02:04:44 +0100

    Added button events

--- js/main.js
+++ js/main.js
@@ -8,6 +8,10 @@ var myElement = document.getElementById("pageSubTitle");
 
 myElement.innerHTML = myString;
 
+var myString = "My awesome project is getting better..";
+
+myElement.innerHTML = myString;
+
 // console loggin'
 
 var w = 42;
@@ -17,8 +21,9 @@ var y = 42;
 
 var z = "42";
 
-var answer = w + z;
-var answer = y + z;
+// fun w/ auto~type
+var answer = w + z;//num+num
+var answer = y + z;//num+string
 
 console.info("Now we're whatever");
 console.log(myElement);
@@ -28,3 +33,18 @@ console.log(y);
 console.log("-------");
 console.log(answer);
 
+//click events and anonymous functions
+
+var myButtons = document.getElementsByClassName("headerbutn");
+
+for (var i=0; i<myButtons.length; i++) {
+	
+	myButtons[i].onclick = function(e)	{
+		
+		e.preventDefault();
+		console.log(e);
+		alert("the " + e.srcElement.localName + " " + e.srcElement.className + " was clicked!!!");
+		
+	}
+	
+}
